7611fb38007405f2cb25d475f66c32216e093120
AranzaMxz <amunozc2002@alumno.ipn.mx>
2024-04-28 23:48:25 -0600

    Update insertNums.py
    Now the user can insert the numbers wherever want it

--- src/Data/insertNums.py
+++ src/Data/insertNums.py
@@ -1,23 +1,29 @@
 from Data.conditions import checkInput
+from Board.matrix import printBoard
 
 def replaceValue(values, position, newValue):
     # values == position + " " -> return a matrix with booleans (true if position + " " is on the board)
     # values[...] -> replaces with the newValue where is true
     values[values == position + " "] = newValue
-    print(values)
+    #print(values)
 
 def getPosition(values):
     # First we ask about the blank space
     while True:
         spaceEmpty = input("What space do you want to leave blank? ")
-        print("you chose " + spaceEmpty)
         # Replace the value in the space selected if is accepted and free
         if checkInput(values, spaceEmpty):
-            replaceValue(values, spaceEmpty, " ")
+            replaceValue(values, spaceEmpty, "  ")
+            printBoard(len(values), values)
             break
-
-
-    #number = int(input(f"Enter a number between 1-{len(values)**2 -1}"))
+    for i in range(len(values)**2 - 1):
+        while True:
+            selection = input(f"\nWhere do you wanna put the {i + 1} ?: ")
+            if checkInput(values, selection):
+                num = str(i + 1 ) + " "
+                replaceValue(values, selection, num)
+                printBoard(len(values), values)
+                break
 
 def insert(values):
     getPosition(values)
