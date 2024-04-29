84e37903f9f71481c6ebbaf9db9f1b24ca50eefb
AranzaMxz <amunozc2002@alumno.ipn.mx>
2024-04-28 22:22:55 -0600

    Update
    Create the board with a dynamic size

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from Data.matrixInfo import getBoardSize
+from Data.matrix import getBoard
+print("\n\t      N U M B E R   P U Z Z L E\n")
+
+# Get the board size
+sizeBoard = getBoardSize()
+# Print the board
+board = getBoard(sizeBoard)
