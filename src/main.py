e1496201628a1b028c22614626708b5d38350fda
AranzaMxz <amunozc2002@alumno.ipn.mx>
2024-04-28 23:47:35 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from Data.matrixInfo import getBoardSize
-from Data.matrix import getBoard
+from Board.matrixInfo import getBoardSize
+from Board.matrix import getBoard
+from Data.insertNums import insert
+
+print("\t---------------------------------------")
 print("\n\t      N U M B E R   P U Z Z L E\n")
+print("\t---------------------------------------\n")
 
 # Get the board size
 sizeBoard = getBoardSize()
 # Print the board
-board = getBoard(sizeBoard)
+values = getBoard(sizeBoard)
+
+# Inserts the number in the position what the player wants
+insert(values)
+
+
