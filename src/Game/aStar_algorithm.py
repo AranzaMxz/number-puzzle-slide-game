cb4219ad9f804123a94ae02f4a48e498f18e7b9e
AranzaMxz <amunozc2002@alumno.ipn.mx>
2024-04-30 00:01:55 -0600

    Updated A*
    Now prints every move

--- src/Game/aStar_algorithm.py
+++ src/Game/aStar_algorithm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import heapq
 from Game.heuristic import heuristicFunc
 from Game.Moves import generateMoves
 from Board.matrix import printBoard
@@ -6,39 +7,46 @@ from Board.matrix import printBoard
 def aStar(start, goal, goalsPosition):
     cont = 0
     # Find the position of blank space
-    blank_pos = np.where(matrix == "  ")
+    blank_pos = np.where(start == "  ")
     
 
     # Saves the nodes to explore (state, g, h, path)
-    openList = [(start, 0, heuristicFunc(start, goal, goalsPosition), None, blank_pos)]
+    openList = [(0, 0, heuristicFunc(start, goal, goalsPosition), start, [], blank_pos)]
+    heapq.heapify(openList) # converts the openList to a heap
     # Saves the expored nodes
-    closeList = []
+    closeList = set()
 
     # While there's nodes to explore  
     while openList:
         # x[1] -> g      x[2] -> h
         # f = g + h
-        matrix, g, h, path, blank_pos= min(openList, key=lambda x: x[1] + x[2])
+        _, g, h, matrix, path, blank_pos = heapq.heappop(openList)  # Pop the node with the smallest f value
+
         if np.array_equal(matrix, goal): # We find a solution
-            path = []
-            while matrix is not None:
-                path.append(matrix)
-                matrix = next((node[3] for node in openList + closeList if np.array_equal(node[0], matrix)), None)
-            for step in path[::-1]:
-                cont += 1
-                printBoard(len(step), step)
-            print(f"\nTotal steps: {cont -1}")
+            path.append(matrix)
+            for i, step in enumerate(path):
+                if i == 0:
+                    print("\t     Start", end="")
+                else:
+                    cont += 1
+                    print(f"\t     Step {cont}", end="")
+                printBoard(len(start), step)
+            print(f"Total steps: {cont}")
             return True
-        openList.remove((matrix,g,h, path, blank_pos)) # Removes the explored node
-        closeList.append((matrix, g, h, path, blank_pos)) # adds the explored node
+        #openList.remove((matrix,g,h, path, blank_pos)) # Removes the explored node
+        closeList.add(tuple(matrix.flatten())) # adds the explored node
 
         # Generates all the paths from the current matrix
         for move in generateMoves(matrix, blank_pos):
             # We verificate the state is new or has being explored
             # if the x state is equals to state move -> true
-            if not any(np.array_equal(x[0], move) for x in closeList):
+            if tuple(move.flatten()) not in closeList:
+                new_blank_pos = np.where(move == "  ")
+                f = g + 1 + heuristicFunc(move, goal, goalsPosition)
+                newPath = path.copy()
+                newPath.append(matrix) # Adds the matrix as a new way
                 # g + 1 -> because we explored a new state
-                openList.append((move, g+1, heuristicFunc(move, goal, goalsPosition), matrix, blank_pos))
+                heapq.heappush(openList, (f, g+1, heuristicFunc(move, goal, goalsPosition), move, newPath, new_blank_pos))
                 
     return False
 
--- src/Game/algorithm.py
+++ src/Game/algorithm.py
@@ -5,7 +5,7 @@ def algorithm(matrix, goalMatrix, goalsPositions):
     print("\t\n Better way\n")
     startTime = time.time()
     if not aStar(matrix, goalMatrix, goalsPositions):
-          print("\nThere's no solution\n")
+          print("\nThere's no a solution\n")
     endTime = time.time ()
     totalTime = endTime - startTime
     print(f"Time to find a solution: {totalTime} second") 
